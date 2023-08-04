e6dd3cefad4109dbb482e9c81a7571fcaac0f15f
Guddu Kumar <141417202+Guddukrm17@users.noreply.github.com>
2023-08-04 19:38:11 +0530

    Add files via upload

---
+++ 02_arithmetic.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<math.h>
+
+int main()
+{
+    int a = 8;
+
+    int b = 2;
+    printf("The value of a+b is:%d\n", a + b);
+    printf("The value of a-b is:%d\n", a - b);
+    printf("The value of a*b is:%d\n", a * b);
+    printf("The value of a % b is:%d\n", a % b);
+    //thik to remember ,no operator is assumed to be present 
+
+    //there is no operator to perform exponentiation in C
+ printf("The use of a exponential of b is:%f\n",pow(a,b));
+ printf("The value of sum is:%d\n",6+3);
+ printf("The value of sum is:%f\n",6+3.5);
+
+    return 0;
+}
